ccace6254e90be910c7a67666f73b45b184bfd04
paulo sandim <paulo@cacdigital.com.br>
2021-04-08 22:12:06 -0300

    verifica elemento visivel

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,9 @@
 Before do
   driver.start_driver
   driver.manage.timeouts.implicit_wait = 10
+
+  # Evento de clique no botão Começar
+  find_element(xpath: "//android.widget.Button[@text='COMEÇAR']").click
 end
 
 After do |scenario|
