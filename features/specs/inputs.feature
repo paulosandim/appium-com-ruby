d4f4fd92319583455d796f48de2e91ac0dc1965b
paulo sandim <paulo@cacdigital.com.br>
2021-04-16 20:10:32 -0300

    marcando os checkboxes

--- features/specs/inputs.feature
+++ features/specs/inputs.feature
@@ -2,8 +2,30 @@
 
 Funcionalidade: Inputs
 
+    @radio
     Cenário: Radio Button
 
         Dado que acesso a tela Botões de Radio
         Quando eu escolho a opção Ruby
         Então esta opção deve ser marcada
+
+    @checkbox
+    Cenário: Checkbox
+
+        Dado que acesso a tela Checkbox
+        Quando eu marco a opção Ruby
+        Então esta opção deve estar marcada
+
+    @check_techs
+    Cenário: Todas as techs que usam Appuium
+
+        Dado que acesso a tela Checkbox
+        Quando eu marco as seguintes techs:
+            |tech|
+            |Ruby|
+            |Java|
+            |Python|
+            |Javascript|
+            |C#|
+            |Robot Framework|
+        Então todas essas opções devem estar marcadas
--- features/steps/inputs_steps.rb
+++ features/steps/inputs_steps.rb
@@ -13,3 +13,39 @@ Então('esta opção deve ser marcada') do
     @radio_result = find_element(xpath: @radio_locator) # houve mudança de estatus
     expect(@radio_result.checked).to eql "true"
 end
+
+Dado("que acesso a tela Checkbox") do
+    @nav.tap_hamburger
+    @nav.tap_by_text("INPUTS")
+    @nav.tap_by_text("CHECKBOX")
+end
+  
+
+Quando("eu marco a opção Ruby") do
+    @check_locator = "//android.widget.CheckBox[contains(@text, 'Ruby')]"
+    find_element(xpath: @check_locator).click
+end
+
+Então("esta opção deve estar marcada") do
+    @check_result = find_element(xpath: @check_locator)
+    expect(@check_result.checked).to eql "true"
+end
+
+Quando("eu marco as seguintes techs:") do |table|
+    @techs = table.hashes
+    # log(@techs)
+
+    @techs.each do |item|
+        # log(item["tech"])
+        check_locator = "//android.widget.CheckBox[contains(@text, '#{item["tech"]}')]"
+        find_element(xpath: check_locator).click
+    end
+end
+
+Então("todas essas opções devem estar marcadas") do
+    @techs.each do |item|
+    check_locator = "//android.widget.CheckBox[contains(@text, '#{item["tech"]}')]"
+    check_result = find_element(xpath: check_locator)
+    expect(check_result.checked).to eql "true"
+    end
+end
